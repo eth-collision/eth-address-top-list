9fe6010b05fd0c8f9b7aef03ae7f09fe97a23ef7
smallyunet <dev.wangyu@gmail.com>
2023-01-11 16:37:33 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# eth-address-top-list
+# ETH address top list
+
+Ethereum address balance top 10,000
+
+## Usage
+
+```
+pip install -r requirements.txt
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time 
+
+for i in range(1, 101):
+    url = "https://etherscan.io/accounts/" + str(i) + "?ps=100"
+    print(url)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--enable-javascript")
+    options.add_argument("--enable-cookies")
+
+    browser = webdriver.Chrome(chrome_options=options)
+    browser.get(url)
+
+    time.sleep(3) 
+
+    html_source = browser.page_source
+    html_soup: BeautifulSoup = BeautifulSoup(html_source, 'html.parser')
+
+    info = html_soup.find_all("a")
+    for list in info:
+        for l in list:
+            t = l.get_text()
+            if t.startswith("0x"):
+                with open("address.txt", "a") as file:
+                    file.write(t + "\n")
+
+    time.sleep(3)
+
